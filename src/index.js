942cdac17a9c0d87e9b41c52de4ad2428e4130c8
Benoit d'Oncieu <bdoncieu@gmail.com>
2016-11-28 14:46:35 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+var pcap = require('pcap');
+var utils = require('./src/utils');
+var player = require('play-sound')(opts = {});
+var pcapSession = pcap.createSession('en4');
+var moment = require('moment');
+var axios = require('axios');
+
+function timestamp() {
+  return '[' + moment().format("DD/MM/YYYY hh:mm:ss") + ']';
+}
+
+listenForDashPress('ac:63:be:75:b9:01', function() {
+  console.log(timestamp(), "Pressed");
+  axios.get("https://maker.ifttt.com/trigger/ariel-dash/with/key/b-J9EQ4GHD5v1VksU4Yu-T");
+  player.play('doorbell.wav');
+});
+
+
+function listenForDashPress(dashbutton, handler) {
+  pcapSession.on('packet', function(rawPacket) {
+    try {
+      var packet = pcap.decode.packet(rawPacket);
+      var dadd = packet.payload.shost.addr;
+      var mac = utils.dadd2hadd(dadd);
+
+      if (mac === dashbutton && utils.throttled()) {
+        handler();
+      }
+    } catch (e) {
+      // ignore
+    }
+  });
+}
